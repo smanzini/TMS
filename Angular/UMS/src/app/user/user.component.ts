8eac42bd6bea86a5910655bb4ea4faf50d9d0bdd
Stefano <smanzini71@gmail.com>
2020-07-02 15:34:56 +0200

    24. prima di Event Emitter 2 luglio 15.34

--- Angular/UMS/src/app/user/user.component.ts
+++ Angular/UMS/src/app/user/user.component.ts
@@ -11,8 +11,7 @@ import { Component, EventEmitter, OnInit, Input, Output } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 @Input ('user-data') user // (23) occorre dichiarare la variabile user nel componente ts del componente figlio e definirla di tipo input cioè che riceve dati dall'esterno; definisco un alias 'user-data' perché se un domani andrò a cambiare il nome della variabile user, l'alias user-data non cambierà
-//@Output ('onDeleteUser') userDeleted new EventEmitter();
-//@Output ('onDeleteUser') userDelete new EventEmitter();// (34) dichiaro la variabile output per definire un evento esterno (userDelete) con un alias 'onDeleteuser' e inizializzo la variabile come un evento new (perché è una nuova istanza) di EventEmitter che è un gestore eventi che si trova in Angular core
+//@Output ('onDeleteUser') userDelete new EventEmitter(); (34) dichiaro la variabile output per definire un evento esterno (userDelete) con un alias 'onDeleteuser' e inizializzo la variabile come un evento new (perché è una nuova istanza) di EventEmitter che è un gestore eventi che si trova in Angular core
   
 constructor(private userService: UserService) { } //(28) se voglio eliminare uno user, devo inserire come dipendenza nel constructor la classe userService di tipo UserService
 
@@ -22,6 +21,6 @@ deleteUser (user){ //(27) aggiungo un metodo deleteUser che riceve il valore use
   alert ("Conferma cancellazione prenotazione per " + this.user.name + " " + this.user.lastname)
    // (35) creo l'evento userDelete con il comando emit che richiama il gestore eventi EventEmitter e cancello this.user; ora devo ASCOLTARE questo evento nel componente padre in users.component.html e quando accadrà questo evento sarà il padre che chiamerà il metodo 
 
-  this.userService.deleteUser(this.user); //(32) chiamo il metodo deleteUser (dalla classe userService per cancellare this.user) (33) faccio richiamare il metodo direttamente dal componente padre (users)
+  this.userService.deleteUser(this.user); //(32) chiamo il metodo deleteUser (dalla classe userService per cancellare this.user) (33) NEW faccio richiamare il metodo direttamente dal componente padre (users)
               }
                                             }
