58ba4beca6d0b2f4bd925c985ee21f3080db9563
Stefano <smanzini71@gmail.com>
2020-07-14 22:02:00 +0200

    ngModel complete 14 luglio

--- Angular/UMS/src/app/app.module.ts
+++ Angular/UMS/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component'; //con l’autocomplete l’edito
 import { UsersComponent } from './users/users.component';
 import { UserComponent } from './user/user.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
-//
+import {FormsModule} from '@angular/forms'; //(74) occorre aggiungere anche import FormsModule da @angular/forms per avere accesso a tutta la parte di form 
 
 @NgModule({
   declarations: [
@@ -20,7 +20,8 @@ import { UserDetailComponent } from './user-detail/user-detail.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule //(74) occorre aggiungere anche import FormsModule da @angular/forms
   ],
   providers: [UserService], // (17) aggiungo nei providers UserSevice
   bootstrap: [AppComponent],
--- Angular/UMS/src/app/user-detail/user-detail.component.ts
+++ Angular/UMS/src/app/user-detail/user-detail.component.ts
@@ -1,5 +1,6 @@
+import { UserInterface } from './../interfaces/user-interface';
 //(50) creo da riga di comando il componente user-detail che serve per le operazioni di modifica sulla destra della tabella, con il comando da terminale ng generate component user-detail (o abbreviato ng g c user-detail)
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-detail', //(51) leggo qui con che nome è stato creato il componente, ovvero il selettore 'app-user-detail' e questo sarà il tag che dovrò richiamare nel mio app.component.html
@@ -7,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent implements OnInit {
-
-  constructor() { }
+@Input()user: UserInterface; // (72) devo creare una variabile di tipo input per ricevere i dati dal form modifica, quindi user di tipo UserInterface
+  
+constructor() { }
 
   ngOnInit(): void {
   }
