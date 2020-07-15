a6023a194888630aba6378943f6595fee5e481c3
Stefano <smanzini71@gmail.com>
2020-07-15 21:57:06 +0200

    salvare modifiche utente 15 luglio

--- Angular/UMS/src/app/app.component.ts
+++ Angular/UMS/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { UserInterface } from './interfaces/user-interface';
+import { UserInterface, userClass } from './interfaces/user-interface';
 import { UserComponent } from './user/user.component';
 import { Component } from '@angular/core';
 
@@ -8,10 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  showTable: boolean = false; // (84) setto una variabile  showTable (Hidran chiama showForm) di tipo boolean (true or false) per poter mostrare la tabella solo nel momento in cui clicco il pulsante modifica (oppure il nuovo pusante aggiungi utente)
   title = 'TMS';
-  userSelected: UserInterface; //(69) aggiungo userSelected che è di tipo UserInterface (Hidran lo chiama User); (82) 
+  userSelected: UserInterface = new userClass(); //(69) aggiungo userSelected che è di tipo UserInterface (Hidran lo chiama User); (83) instanzio una nuova classe userClass che ho definito in user-interface
   updateUser (user:UserInterface) //(70) chiamo il metodo updateUser perché l'utente che viene passato sarà quello selezionato
    {
-this.userSelected = user;
+    this.showTable = true; //(85) la variabile boolean viene settata a true solo se viene selezionato l'utente
+    this.userSelected = user;
   }
 }
--- Angular/UMS/src/app/interfaces/user-interface.ts
+++ Angular/UMS/src/app/interfaces/user-interface.ts
@@ -1,4 +1,26 @@
+//import { UserInterface } from './user-interface';
 // (40) creo il file interface con Angular
+// (82) creo una nuova classe userClass che esporto per poterla utilizzare in altri files (Hidran lo chiama User) che implementa UserInterface per poter settare l'id degli utenti -> viene importato automaticamente UserInterface MA NON serve, per cui lo tolgo (in alto)
+export class userClass implements UserInterface {
+            id: number;
+            pnr: string;
+            service: string;
+            name: string;
+            lastname: string;
+            from: string;
+            to: string;
+            date: string;
+            constructor () {
+                this.id= 0; // (82) inizializzo tutti i valori a zero o vuoti -> quindi se id è zero allora sto inserendo un nuovo utente, se invece è maggiore di zero, vuol dire che sto modificando un utente
+                this.pnr='';
+                this.date='';
+                this.from='';
+                this.to='';
+                this.lastname='';
+                this.name='';
+                this.service='';
+            }
+}
 export interface UserInterface { //(41) tipicizzo i parametri dell'array -> al posto del nome User utilizzato da Hidran, utilizzo UserInterface
             id: number;
             pnr: string;
